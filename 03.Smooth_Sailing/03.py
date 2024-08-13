a5041be74ade8e43a9c77e152f5483a0eef3bad4
o-aguirre <two.aguirre@gmail.com>
2024-08-13 17:33:39 -0400

    Resolucion Smooth Sailing - isLucky

---
+++ 03.Smooth_Sailing/03.py
@@ -0,0 +1,17 @@
+def solution(n):
+    n = list(str(n))
+    half = int(len(n)/2)
+    f_half = n[:half]
+    s_half = n[half:len(n)]
+    
+    sum_1 = 0
+    sum_2 = 0
+    for i in f_half:
+        sum_1 += int(i)
+    for i in s_half:
+        sum_2 += int(i)
+    
+    if sum_1 == sum_2:
+        return True
+    else:
+        return False
